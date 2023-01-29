45e97661a154ff940e835ff043c98984407fcf21
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2023-01-29 17:33:27 +0900

    なんかした

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -12,12 +9,42 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="p-4 text-center">
-        <h1 className="text-5xl p-2 bg-gray-600 rounded">CSSで遊ぶ</h1>
-        <h2>TODO</h2>
-        <li>ダークモード</li>
-        <li>クラス名共通化</li>
-        <li>github</li>
+      <main className="p-8 text-left w-1/2 mx-auto bg-gray-800 border-gray-700 border shadow-inner">
+        <h1 className="text-5xl">CSSで遊ぶ</h1>
+        <div className="flex gap-2 mt-2">
+          <div className="p-2 rounded-xl bg-gray-600 text-sm">Next v13</div>
+          <div className="p-2 rounded-xl bg-gray-600 text-sm">tailwind</div>
+        </div>
+
+        <h2 className="text-2xl mt-4">TODO</h2>
+        <div className="card">
+          <li>ダークモード</li>
+          <li>クラス名共通化</li>
+          <li>github</li>
+        </div>
+        <h2 className="text-2xl mt-4">トレンド</h2>
+        <div className="card">
+          <li>subgrid</li>
+          <li>writing modes</li>
+          <li>css logical propeties</li>
+          <li>aspect-ratio</li>
+          <li>content-visibility</li>
+          <li>flexbox gap</li>
+          <li>@container</li>
+        </div>
+        <h3 className="mt-4 text-xl">Grid</h3>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-1 card">1</div>
+          <div className="col-span-2 card">2</div>
+          <div className="col-span-2 card">3</div>
+          <div className="col-span-1 card">4</div>
+          <div className="card col-span-3">5</div>
+          <div className="card col-span-2 row-span-4">6</div>
+          <div className="card col-span-1 row-span-1">7</div>
+          <div className="card col-span-1 row-span-1">8</div>
+          <div className="card col-span-1 row-span-1">9</div>
+          <div className="card col-span-1 row-span-1">10</div>
+        </div>
       </main>
     </>
   );
